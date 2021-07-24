3fdbe98679b250c3a3f222c6e7e227d3561f3e5e
SamX23 <skalexsong@gmail.com>
2021-07-24 17:01:13 +0700

    refactor : fix folder names

---
+++ part-1/courseinfo/src/App.js
@@ -0,0 +1,33 @@
+import Content from "./component/Content";
+import Header from "./component/Header";
+import Total from "./component/Total";
+
+function App() {
+  const course = {
+    name: "Almost stack app development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercise: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercise: 7,
+      },
+      {
+        name: "State of a component",
+        exercise: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+}
+
+export default App;
