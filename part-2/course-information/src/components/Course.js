7118d2747ef3ebb285984e53eaba237299b5a6d9
SamX23 <skalexsong@gmail.com>
2021-08-08 22:26:24 +0700

    Update : add total excercise

--- part-2/course-information/src/components/Course.js
+++ part-2/course-information/src/components/Course.js
@@ -1,14 +1,17 @@
 const Course = (course) => {
+  const [state, setstate] = useState(0);
+  let exercise = 0;
   return (
     <>
       <h1>{course.name}</h1>
       <main>
         {course.map((item) => {
-          <p key={item.id}>
+          <p key={item.id} data-set={setstate(state + item.exercises)}>
             {item.name} {item.exercises}
           </p>;
         })}
       </main>
+      <p>Total of {state} excercise.</p>
     </>
   );
 };
